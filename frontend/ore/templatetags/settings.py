9dfccc9339c7eae7a75c246fec94baae9b3eccbb
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-06-06 20:48:13 +0200

    rename

--- frontend/ore/templatetags/settings.py
+++ frontend/ore/templatetags/settings.py
@@ -20,4 +20,4 @@ def setting(name):
 
 @register.assignment_tag
 def get_dev_login():
-    return os.environ.get('DEV_LOGIN', False) is True or settings.DEBUG is True
+    return os.environ.get('ORE_DEV_LOGIN', False) is True or settings.DEBUG is True
